82d666accc82c3c8c8ed0ca8d506a18a84ecbb6a
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-10-20 21:18:45 -0400

    I tested this program and it works now

---
+++ Demo.java
@@ -0,0 +1,19 @@
+public class Demo{
+  public static void printLoop(int n){
+    for(int i = 1; i <= n; i++){
+      for(int j = 0; j <= n-i; j++){
+         System.out.print(i); //prints out rectangular shaped numbers (like the example above)
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int n;
+    if(args.length == 0)
+      n = 5;
+    else
+      n = Integer.parseInt(args[0]);
+    printLoop(n);
+  }
+}
